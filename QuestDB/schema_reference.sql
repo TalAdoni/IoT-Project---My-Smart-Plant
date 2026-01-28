df53c2570149bfe46390f0262cbf600f67fe3aa7
Omri Aviram <omriaviram93@gmail.com>
2026-01-28 16:42:20 +0200

    Delete QuestDB directory

--- QuestDB/schema_reference.sql
+++
@@ -1,7 +0,0 @@
-TABLE sensor_data (
-  plant SYMBOL,         -- Plant Identifier
-  soilRaw LONG,         -- Sensor Raw Value
-  tempC DOUBLE,         -- Temperature
-  humRH DOUBLE,         -- Humidity
-  timestamp TIMESTAMP   -- Time of ingestion
-)
